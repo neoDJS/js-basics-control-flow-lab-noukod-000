6b759a19add7052a026a4529e35a620bc64b7494
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-04 03:03:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ function scuberGreetingForFeet(someValue) {
     greeting = 'No can do.';
   } else if (someValue > 2000 ) {
     greeting = 'I will gladly take your thirty bucks.';
-  } else if (someValue > 400 ) {
-    greeting = 'I will gladly take your thirty bucks.';
   } else if (someValue <= 400) {
     greeting = 'This one is on me!';
+  } else {
+    
   }
 }
