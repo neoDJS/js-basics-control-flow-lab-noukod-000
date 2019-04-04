e84bb750466aa6f973bc85d529c993aa8743025f
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-04 03:23:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your code in this file!
+function scuberGreetingForFeet(someValue) {
+  //this is where we can use conditionals given our argument, someValue
+  //don't forget to return whatever the result is!
+  let greeting = "";
+
+  if (someValue > 2500) {
+    greeting = 'No can do.';
+  } else if (someValue > 2000 ) {
+    greeting = 'I will gladly take your thirty bucks.';
+  } else if (someValue <= 400) {
+    greeting = 'This one is on me!';
+  } else {
+
+  }
+
+  return greeting
+}
+
+
+function ternaryCheckCity(dest) {
+  return dest === 'NYC' ? "Ok, sounds good." : "No go.";
+}
+
+function switchOnCharmFromTip(tip) {
+  let charm = '';
+  switch (tip) {
+    case 'generous':
+      charm = "Thank you so much.";
+      break;
+    case 'not as generous':
+      charm = "Thank you.";
+      break;
+    default:
+      charm = "Bye.";
+  }
+  return charm
+}
